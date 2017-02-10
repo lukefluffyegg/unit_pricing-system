43b7bd77ab80172b5fe09b37df9ac0ddf985eede
luke davies <luke@fluffyegg.com>
2017-02-10 16:46:56 +0000

    first commit

---
+++ _db_config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database config for plugin */
+ include('../wp-config.php');
+
+// Database Variables
+ $username = DB_USER;
+ $password = DB_PASSWORD;
+ $database = DB_NAME;
+ $host     = DB_HOST;
+
+ $db = mysqli_connect($host, $username, $password, $database) or die(mysql_error());
+
+    if(mysqli_connect_errno()) {
+        echo "Failed to connect to MSQL:" . mysqli_connect_errno();
+    }
+?>
---
+++ unit-pricing-system.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Plugin Name: Unit Pricing System
+ * Plugin URI: www.fluffyegg.com
+ * Description: Unit Pricing System is a plugin that allows you to enter custom options/prices for surface areas such as wood per mm
+ * Version: 0.0.1
+ * Author: Fluffy Egg
+ * Author URI: www.fluffyegg.com
+ * Developer: Luke Davies
+ * Developer URI: www.fluffyegg.com
+ * Text Domain: unit-pricing-system
+ * Domain Path: /languages
+ *
+ * Copyright: © 2014-2017 FluffyEgg.
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+ // Exit if directly accessed
+ if(!defined('ABSPATH')) {
+    exit;
+ }
+
+ // Checking to see if WooCommerce is active
+ if(in_array('woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins')))) {
+
+    // Activate the plugin with database support
+    function unit_pricing_system_activate() {
+        include('_db_config.php');
+
+        mysqli_query($db, 
+            "CREATE TABLE IF NOT EXISTS `unit_pricing_system`(
+                `id` int(11) NOT NULL AUTO_INCREMENT,
+                `unit_option_name` VARCHAR NOT NULL,
+                `min_mm` int(11) NOT NULL,
+                `max_mm` mediumint(9) NOT NULL,
+                `price_per_mm` decimal(10,2) NOT NULL,
+                 PRIMARY_KEY(`id`)
+            ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1
+        ");
+    }
+
+     // Run the database query
+     register_activation_hook(__FILE__, 'unit_pricing_system_activate');
+ }
+
